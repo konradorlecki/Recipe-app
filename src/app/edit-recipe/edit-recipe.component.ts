b1797cc8499695e748bfa2cbdc091b3648b68de8
Konrad <konradorlecki6@wp.pl>
2019-12-03 11:36:53 +0100

    ingredients input addition

--- src/app/edit-recipe/edit-recipe.component.ts
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -1,28 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from '../services/firebase.service';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseService} from '../services/firebase.service';
+import {NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-edit-recipe',
   templateUrl: './edit-recipe.component.html',
   styleUrls: ['./edit-recipe.component.scss']
 })
 export class EditRecipeComponent implements OnInit {
-  id;
-  sendToFirebase(recipe){
-    this.firebaseService.createRecipe(recipe).then(
-      res=>{console.log(res)}
-    )
-  }
-  constructor( public firebaseService: FirebaseService) {
+  public ingredients = [{
+    ingredient: '',
+    measure: ''
+  }];
+
+  private id;
+
+
+  constructor(public firebaseService: FirebaseService) {
     this.firebaseService.getRecipes().subscribe(
-      snapshot => {this.id=snapshot.size}
-      )}
-  onSubmit(recipe) {
-
+      snapshot => {
+        this.id = snapshot.size;
+      }
+    );
   }
-
 
   ngOnInit() {
-  console.log(this.id);
+    console.log(this.id);
+  }
+
+  addIngredient() {
+    this.ingredients.push({
+      ingredient: '',
+      measure: ''
+    });
+    console.log('test', 'ingredients', this.ingredients);
+  }
+
+  sendToFirebase(recipe) {
+    this.firebaseService.createRecipe(recipe).then(
+      res => {
+        console.log(res);
+      }
+    );
+  }
+
+  onSubmit(recipe :NgForm) {
+    let value = recipe.value;
+    console.log(recipe.value);
+    let recipeToSend={
+      idMeal:this.id,
+      strMeal: value.name,
+
+    }
   }
 
 }
